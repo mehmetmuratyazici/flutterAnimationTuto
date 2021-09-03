6ff42f2a7bd628af4dabd54344414639ef79a464
mhmtmrtyzc <mhmtmrtyzc@gmail.com>
2021-09-03 15:33:53 +0300

    Warning Message added

--- lib/screens/firstScreen.dart
+++ lib/screens/firstScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class FirstScreen extends StatefulWidget {
-  const FirstScreen({ Key? key , required this.title}) : super(key: key);
+  const FirstScreen({Key? key, required this.title}) : super(key: key);
 
   final String title;
   @override
@@ -13,100 +13,119 @@ class _FirstScreenState extends State<FirstScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: AppBar(
         backgroundColor: Colors.red.shade400,
-        centerTitle: true,  
+        centerTitle: true,
         title: Text(widget.title),
       ),
       body: Stack(
         children: [
           Container(
             decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                  colors: [
-                    Colors.red.shade400,
-                    Colors.red.shade900
-                  ],
-                ),
+              gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Colors.red.shade400, Colors.red.shade900],
+              ),
             ),
           ),
           Container(
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  AnimatedPositioned(
-                    left: !pressed ? -MediaQuery.of(context).size.width * 0.5 -50 : MediaQuery.of(context).size.width * 0.5 * 0.5, 
-                    curve: Curves.bounceOut,
-                    duration: Duration(milliseconds: 500),
-                    child: Container(
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.5 ,
-                          child: Container(
-                              child: Column(
-                                children: [
-                                   TextFormField(
-                                     
-                                     decoration: InputDecoration(
-                                       
-                                       hintText: "Kullanıcı Adı"
-                                     ),
-                                   ),
-                                   TextFormField(
-                                     obscureText: true,
-                                     decoration: InputDecoration(
-                                       hintText: "Password",
-                                     ),
-                                   ),
-                                   Padding(padding: EdgeInsets.all(20),
-                                          child: SizedBox(
-                                            width: 120,
-                                            height: 50,
-                                            child: RaisedButton(
-                                                  color: Colors.red,
-                                                  child: Text("Login"),
-                                                  onPressed: (){
-                                                      setState(() {
-                                                        pressed = !pressed;
-                                                      });
-                                                  }
-                                                ),
-                                          ),
-                                   )
-                                ],
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                AnimatedPositioned(
+                  left: !pressed
+                      ? -MediaQuery.of(context).size.width * 0.5 - 50
+                      : MediaQuery.of(context).size.width * 0.5 * 0.5,
+                  curve: Curves.bounceOut,
+                  duration: Duration(milliseconds: 500),
+                  child: Container(
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: Container(
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        pressed = !pressed;
+                                      });
+                                    },
+                                    icon: Icon(Icons.arrow_back_ios))
+                              ],
+                            ),
+                            TextFormField(
+                              decoration:
+                                  InputDecoration(hintText: "Kullanıcı Adı"),
+                            ),
+                            TextFormField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                hintText: "Password",
                               ),
-                          ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(20),
+                              child: SizedBox(
+                                width: 120,
+                                height: 50,
+                                child: RaisedButton(
+                                    color: Colors.red,
+                                    child: Text("Login"),
+                                    onPressed: () {
+                                      showDialog(
+                                          context: context,
+                                          builder: (context) {
+                                            return AlertDialog(
+                                              title: Text("Uyarı"),
+                                              content: Center(
+                                                child: Text("To be Countinue"),
+                                              ),
+                                              actions: [
+                                                TextButton(
+                                                    onPressed: () {
+                                                      Navigator.pop(context);
+                                                    },
+                                                    child: Text("Ok"))
+                                              ],
+                                            );
+                                          });
+                                    }),
+                              ),
+                            )
+                          ],
                         ),
-                    ),
-                  ),
-                  AnimatedPositioned(
-                    left: pressed ? -300 : 0, 
-                    //top:  pressed ? MediaQuery.of(context).size.width * 0.5 -25 : 0, 
-                    duration: Duration(milliseconds: 500),
-                    curve: Curves.easeIn,
-                    child: Container(
-                      
-                      child: SizedBox(
-                        width: 100,
-                        height: 50,
-                        child:RaisedButton(
-                            color: Colors.white,
-                            textColor: Colors.red,
-                            child: Text("Let's Go!"),
-                            onPressed: (){
-                              setState(() {
-                                pressed = !pressed;
-                              });
-                            },
-                          ),
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+                AnimatedPositioned(
+                  left: pressed ? -300 : 0,
+                  //top:  pressed ? MediaQuery.of(context).size.width * 0.5 -25 : 0,
+                  duration: Duration(milliseconds: 500),
+                  curve: Curves.easeIn,
+                  child: Container(
+                    child: SizedBox(
+                      width: 100,
+                      height: 50,
+                      child: RaisedButton(
+                        color: Colors.white,
+                        textColor: Colors.red,
+                        child: Text("Let's Go!"),
+                        onPressed: () {
+                          setState(() {
+                            pressed = !pressed;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
+          ),
         ],
       ),
     );
