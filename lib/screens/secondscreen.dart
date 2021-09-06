7e27e19f3751e99a1a8f22a12ebda895cea3ed25
sumeyyenuremir <sumeyyenr19@gmail.com>
2021-09-06 13:35:32 +0300

    create random phone number to listview in second secreen

--- lib/screens/secondscreen.dart
+++ lib/screens/secondscreen.dart
@@ -1,3 +1,6 @@
+
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Secondscreen extends StatefulWidget {
@@ -8,13 +11,30 @@ class Secondscreen extends StatefulWidget {
 }
 
 class _SecondscreenState extends State<Secondscreen> {
-  List<String> listItems = List<String>.generate(100, (i) => 'Item $i');
-
+  List<String>? listItems;
+  void setPhoneList(){
+    
+    Random random=new Random();
+    String aa = "";
+    listItems = List<String>.generate(100, (index) => "05"
+                                                      + getRandNuber(random, 2)
+                                                      + getRandNuber(random, 3)
+                                                      + getRandNuber(random, 2)
+                                                      + getRandNuber(random, 2)
+                                                      );
+  }
+  String getRandNuber(Random rn, num width){
+    String result = rn.nextInt(pow(10, width).toInt()).toString();
+    return result.length < width ? result.padLeft(1,"0") : result;
+   
+  }
+   
   void showMessage() {}
 
   @override
   Widget build(BuildContext context) {
     const title = 'Grid List';
+    setPhoneList();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: title,
@@ -25,16 +45,25 @@ class _SecondscreenState extends State<Secondscreen> {
           title: Text("Recents"),
         ),
         body: ListView.builder(
-            itemCount: listItems.length,
+            itemCount: listItems!.length,
             itemBuilder: (context, index) {
               return ListTile(
                 title: Text("Name Surname"),
-                subtitle: Text(listItems[index]),
+                subtitle: Text(listItems![index]),
                 trailing: IconButton(
                   highlightColor: Colors.transparent,
                   hoverColor: Colors.transparent,
                   icon: Icon(Icons.phone),
-                  onPressed: () {},
+                  onPressed: () {
+                    showDialog(
+                      context: context, 
+                      builder:(context){
+                        return AlertDialog(
+                          title: Text(""),
+                        );
+                      }
+                      );
+                  },
                 ),
                 leading: IconButton(
                   highlightColor: Colors.transparent,
