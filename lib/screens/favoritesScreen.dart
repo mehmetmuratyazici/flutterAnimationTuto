361f2a801d7baf6c9ff332ef958f0df2ea840173
mhmtmrtyzc <mhmtmrtyzc@gmail.com>
2021-09-24 09:29:02 +0300

    animation design

--- lib/screens/favoritesScreen.dart
+++ lib/screens/favoritesScreen.dart
@@ -5,97 +5,121 @@ import 'package:animation_tuto/widget/people.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
 class FavoritesScreen extends StatefulWidget {
   const FavoritesScreen({Key? key}) : super(key: key);
 
   @override
   _FavoritesScreenState createState() => _FavoritesScreenState();
 }
+
 void changeScreen(BuildContext _context, Widget _widget) {
   Navigator.push(_context, MaterialPageRoute(builder: (_context) => _widget));
 }
 
-
 class _FavoritesScreenState extends State<FavoritesScreen> {
- 
-
-   
+  bool editing = false;
   @override
   Widget build(BuildContext context) {
-    
     final fav = Provider.of<PeopleProvider>(context);
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.red.shade400,
-          centerTitle: true,
-          title: Text("Favorites"),          
-          actions: [
-            IconButton(
+        backgroundColor: Colors.red.shade400,
+        centerTitle: true,
+        title: Text("Favorites"),
+        actions: [
+          IconButton(
+            highlightColor: Colors.transparent,
+            hoverColor: Colors.transparent,
+            icon: Icon(Icons.add),
+            onPressed: () {
+              changeScreen(context, People());
+            },
+          ),
+          IconButton(
+              onPressed: () {},
               highlightColor: Colors.transparent,
               hoverColor: Colors.transparent,
-              icon: Icon(Icons.add), 
-                          
-              onPressed: () {
-                changeScreen(context,People());
-              },
-            ),
-           
-             IconButton(onPressed: (){
-              
-             },
-             highlightColor: Colors.transparent,
-             hoverColor: Colors.transparent,
-             iconSize: 50,
-             icon: Text("Düzenle",))
-           
-          ],
-          
-         
-          ),
+              iconSize: 50,
+              icon: Text(
+                "Düzenle",
+              ))
+        ],
+      ),
       body: ListView.builder(
-      
-      
         itemCount: fav.favNameList!.length,
         itemBuilder: (context, index) {
-        
-          return Dismissible(
-            key: Key(fav.favNameList![index]), 
-            onDismissed: (direction){
-              setState(() {
-                 
-              fav.favNameList!.removeAt(index);
-                });
-               
-            },
-             background: Container(color: Colors.red),
-            
-            child: ListTile(            
-          
-            title: Text(
-                fav.favNameList![index] + ' ' + fav.favSurnameList![index]),
-            subtitle: Text(fav.favNumberList![index]),
-            
-            trailing: IconButton(
-              highlightColor: Colors.transparent,
-              hoverColor: Colors.transparent,
-              icon: Icon(Icons.phone),
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Text(fav.favNumberList![index]),
-                    );
-                  },
-                );
-              },
-            ),
-          )
+          return Stack(
+            children: [
+              AnimatedPositioned(
+                left: editing ? 50 : 0,
+                duration: Duration(seconds: 1),
+                curve: Curves.bounceInOut,
+                child: Container(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: ListTile(
+                      title: Text(fav.favNameList![index] +
+                          ' ' +
+                          fav.favSurnameList![index]),
+                      subtitle: Text(fav.favNumberList![index]),
+                      trailing: IconButton(
+                        highlightColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        icon: Icon(Icons.phone),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text(fav.favNumberList![index]),
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           );
+
+          // Dismissible(
+          //   key: Key(fav.favNameList![index]),
+          //   onDismissed: (direction){
+          //     setState(() {
+
+          //     fav.favNameList!.removeAt(index);
+          //       });
+
+          //   },
+          //    background: Container(color: Colors.red),
+
+          //   child: ListTile(
+
+          //   title: Text(
+          //       fav.favNameList![index] + ' ' + fav.favSurnameList![index]),
+          //   subtitle: Text(fav.favNumberList![index]),
+
+          //   trailing: IconButton(
+          //     highlightColor: Colors.transparent,
+          //     hoverColor: Colors.transparent,
+          //     icon: Icon(Icons.phone),
+          //     onPressed: () {
+          //       showDialog(
+          //         context: context,
+          //         builder: (context) {
+          //           return AlertDialog(
+          //             title: Text(fav.favNumberList![index]),
+          //           );
+          //         },
+          //       );
+          //     },
+          //   ),
+          // )
+          // );
         },
       ),
-     
     );
   }
 }
